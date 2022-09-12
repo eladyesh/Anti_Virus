e8b4095251213fdb88a08a8105d25d5f7bce966b
eladyesh <eladyesh24@gmail.com>
2022-09-12 22:35:01 +0300

    added pch.h, pch.cpp and continues adding winapi functions

---
+++ pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <windows.h>
+#include <iostream>
+#include <map>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <sstream>
