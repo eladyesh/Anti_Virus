8ccddf517203f12b71273c45ccfc047a112ec37d
eladyesh <102996033+eladyesh@users.noreply.github.com>
2022-09-23 17:08:51 +0300

    Create receiver.py

---
+++ send_to_vm/receiver.py
@@ -0,0 +1,46 @@
+import struct
+from socket import socket, AF_INET, SOCK_STREAM
+import os
+
+FILE_NAME_TO_SAVE = "virus.exe"
+FILE_NAME_TO_SEND = "LOG.txt"
+
+server_sock = socket(AF_INET, SOCK_STREAM)
+server_sock.bind(("0.0.0.0", 9999))
+server_sock.listen()
+print("Server now listening...")
+
+sock, _ = server_sock.accept()
+
+# receiving file
+file_to_recv_size = struct.unpack("I", sock.recv(struct.calcsize("I")))[0]
+file = b''
+while len(file) < file_to_recv_size:
+    try:
+        file_fragment = sock.recv(file_to_recv_size - len(file))
+    except:
+        print("error in recv")
+        quit()
+    if not file_fragment:
+        print("error in data")
+        quit()
+    file = file + file_fragment
+
+with open(FILE_NAME_TO_SAVE, "wb") as f:
+    f.write(file)
+
+os.system('..')
+os.system('Z:\D\Cyber\YB_CYBER\project\FinalProject\poc_start\poc_start')
+os.startfile('Z:\D\Cyber\YB_CYBER\project\FinalProject\poc_start\poc_start\poc_start.exe')
+
+while not os.path.exists("LOG.txt"):
+    pass
+
+with open(FILE_NAME_TO_SEND, "rb") as f:
+    file_to_send_data = f.read()
+
+sock.sendall(struct.pack("I", len(file_to_send_data)) + file_to_send_data)
+print("Send report to local machine")
+
+sock.close()
+server_sock.close()
