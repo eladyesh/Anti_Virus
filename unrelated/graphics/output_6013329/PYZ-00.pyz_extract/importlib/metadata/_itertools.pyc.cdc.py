4803f6795d9c2852d8d4ba3ec73e6bbdc14034f1
eladyesh <eladyesh24@gmail.com>
2023-03-05 11:14:27 +0200

    continuing graphics and python

--- unrelated/graphics/output_6013329/PYZ-00.pyz_extract/importlib/metadata/_itertools.pyc.cdc.py
+++
@@ -1,8 +0,0 @@
-
-from itertools import filterfalse
-
-def unique_everseen(iterable, key = (None,)):
-    '''List unique elements, preserving order. Remember all elements ever seen.'''
-    pass
-# WARNING: Decompyle incomplete
-
--- unrelated/graphics/output_6013329/PYZ-00.pyz_extract/mimetypes.pyc.cdc.py
+++
@@ -1,7 +0,0 @@
-
-__doc__ = 'Guess the MIME type of a file.\n\nThis module defines two useful functions:\n\nguess_type(url, strict=True) -- guess the MIME type and encoding of a URL.\n\nguess_extension(type, strict=True) -- guess the extension for a given MIME type.\n\nIt also contains the following, for tuning the behavior:\n\nData:\n\nknownfiles -- list of files to parse\ninited -- flag set when init() has been called\nsuffix_map -- dictionary mapping suffixes to suffixes\nencodings_map -- dictionary mapping suffixes to encodings\ntypes_map -- dictionary mapping suffixes to types\n\nFunctions:\n\ninit([files]) -- parse a list of files, default knownfiles (on Windows, the\n  default values are taken from the registry)\nread_mime_types(file) -- parse one file, return a dictionary or None\n'
-import os
-import sys
-import posixpath
-import urllib.parse as urllib
-# WARNING: Decompyle incomplete
